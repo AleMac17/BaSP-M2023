4fa7ee23892430b063fad28e88e959adc99bbb64
AleMac17 <alemacia17@gmail.com>
2023-04-16 11:58:35 -0300

    BaSP-W05-2023: Finish exercise 01

---
+++ Week-05/exercises/01-variables-and-operators.js
@@ -0,0 +1,17 @@
+var num1 = 1;
+var num2 = 2;
+var resultado;
+resultadoNum = num1 + num2;
+console.log(resultadoNum)
+
+var string1 = "Hola";
+var string2 = "Mundo";
+var resultadoString;
+resultadoString = string1 + string2;
+console.log(resultadoString)
+
+var stringLength1 = string1.length;
+var stringLength2 = string2.length;
+var resultadoStringLength 
+resultadoStringLength = stringLength1 + stringLength2;
+console.log(resultadoStringLength)
